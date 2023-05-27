99c4a80bc16e0fa94ea0bfb266f4ae77951e1ed3
EduardoCoelho123 <134801165+EduardoCoelho123@users.noreply.github.com>
2023-05-27 15:15:13 -0300

    feat: create relogio digital

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const horas = document.getElementById("horas");
+const minutos = document.getElementById("minutos");
+const segundos = document.getElementById("segundos");
+
+const relogio = setInterval(function time() {
+    let datadehoje = new Date();
+    let hr = datadehoje.getHours();
+    let min = datadehoje.getMinutes();
+    let seg = datadehoje.getSeconds();
+
+
+    if (hr < 10) hr = "0" + hr;
+    if (min < 10) min = "0" + min;
+    if (seg < 10) seg = "0" + seg;
+
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+
+
+
+})
